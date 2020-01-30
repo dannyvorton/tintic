3d0963319c6da33116a5461f546e3e9bda8d83af
Aaron <10297223+aarondevon@users.noreply.github.com>
2020-01-29 21:31:51 -0600

    add old project files

---
+++ contact.php
@@ -0,0 +1,1 @@
+<?php  $to = "me@aarondevon.com";  $subject = "Contact Us Form";  $email = $_REQUEST['email'] ;  $message = $_REQUEST['message'] ;  $headers = "From: $email";  $sent = mail($to, $subject, $message, $headers) ;  if($sent)  {print "Your mail was sent successfully"; } else  {print "We encountered an error sending your mail"; } ?>
