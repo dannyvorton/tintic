eb858c99681c816316d4a5d609b465091773f1a6
dannyvorton <dan@tintic.co>
2020-04-18 14:59:48 -0600

    Deleting file

--- contact.php
+++
@@ -1,1 +0,0 @@
-<?php  $to = "me@aarondevon.com";  $subject = "Contact Us Form";  $email = $_REQUEST['email'] ;  $message = $_REQUEST['message'] ;  $headers = "From: $email";  $sent = mail($to, $subject, $message, $headers) ;  if($sent)  {print "Your mail was sent successfully"; } else  {print "We encountered an error sending your mail"; } ?>
